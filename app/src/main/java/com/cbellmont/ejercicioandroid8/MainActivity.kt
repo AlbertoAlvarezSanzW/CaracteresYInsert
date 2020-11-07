44efc878adcb3f605d23ca9e017ba923f708a104
Alberto <alberto.alvarez.sanz@gmail.com>
2020-11-07 23:42:36 +0100

    commit

--- app/src/main/java/com/cbellmont/ejercicioandroid8/MainActivity.kt
+++ app/src/main/java/com/cbellmont/ejercicioandroid8/MainActivity.kt
@@ -16,9 +16,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addListenerToTextView()
         addListenerToButton()
         addListenerToEditText()
-        addListenerToTextView()
+
     }
 
     private fun addListenerToTextView() {
